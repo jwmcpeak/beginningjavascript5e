cbb2cea139028fb3a04515b11384f616b6694633
Jeremy McPeak <jwmcpeak@gmail.com>
2019-10-02 21:05:29 -0500

    removed files not needed for site

--- examples/draggable-polyfill.js
+++
@@ -1,8 +0,0 @@
-﻿document.attachEvent("onselectstart", function (e) {
-    var target = e.srcElement;
-
-    if (target.getAttribute("draggable")) {
-        e.returnValue = false;
-        target.dragDrop();
-    }
-});
